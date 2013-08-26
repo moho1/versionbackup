862699c15a123caca08c1bd44255d40ca29e5a2a
Moritz Holtz <moritz.holtz@koeln.de>
2013-08-26 22:14:35 +0200

    kleine Veränderungen am Client

--- client/client.py
+++ client/client.py
@@ -3,35 +3,38 @@
 # Author: Moritz Holtz
 
 import os
+import sys
 import hashlib
 import sqlite3
-            
+
 def index(path):
     """The sha512 hashs are being calculated and inserted into the database
 """
     conn = sqlite3.connect("tmp.db")
-    conn.text_factory = str
     c = conn.cursor()
     c.execute("drop table if exists sums")
     c.execute("create table sums (sum text, filename text)")
     for root, dirs, files in os.walk(path):
         for name in files:
+            filename = root + "/" + name
             try:
-            	data = open(root + "/" + name, "r")
-            	print(root + "/" + name)
-            	filename = root + "/" + name
-            	filehash = hashlib.sha512(data.read()).hexdigest()
+                data = open(filename, "r")
+                print(filename)
+                filehash = hashlib.sha512(data.read()).hexdigest()
 
             except IOError:
-		print("IOError")
-                continue
+                print("IOError")
+                return
 
             except MemoryError:
                 print("MemoryError")
-                continue
+                return
 
-            c.execute("insert into sums values ( ?, ? )", (filehash, filename))
+            c.execute("insert into sums values ( ?, ? )", (filehash, buffer(filename)))
             data.close()
     conn.commit()
     conn.close()
+
+if __name__ == "__main__":
+    index(sys.argv[1])
 
