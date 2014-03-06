daccd2eab5f208f9de785d35c048e2fb4cb06c83
Moritz Holtz <moritz.holtz@rwth-aachen.de>
2014-03-06 22:11:20 +0100

    client erweitert, brauchbar?

--- client/client.py
+++ client/client.py
@@ -6,14 +6,22 @@ import os
 import sys
 import hashlib
 import sqlite3
+import socket
 
-def index(path):
+Fail=False
+
+def index(path, tmpdb):
     """The sha512 hashs are being calculated and inserted into the database
 """
-    conn = sqlite3.connect("tmp.db")
+    conn = sqlite3.connect(tmpdb)
     c = conn.cursor()
-    c.execute("drop table if exists sums")
-    c.execute("create table sums (sum text, filename text)")
+
+    c.execute("drop table if exists files")
+    c.execute("drop table if exists dirs")
+
+    c.execute("create table files (sum text, filename text)")
+    c.execute("create table dirs (dirname text)")
+
     for root, dirs, files in os.walk(path):
         for name in files:
             filename = root + "/" + name
@@ -23,18 +31,60 @@ def index(path):
                 filehash = hashlib.sha512(data.read()).hexdigest()
 
             except IOError:
-                print("IOError")
-                return
+#               print("IOError")
+                sys.__stderr__.write("IOError in file " + filename + "\n")
+                Fail=True
+                continue
 
             except MemoryError:
-                print("MemoryError")
-                return
+#                print("MemoryError")
+                sys.__stderr__.write("MemoryError in file " + filename + "\n")
+                fail=True
+                continue
 
-            c.execute("insert into sums values ( ?, ? )", (filehash, buffer(filename)))
+            c.execute("insert into files values ( ?, ? )", (filehash, buffer(filename)))
             data.close()
+
+        for name in dirs:
+            dirname = root + "/" + name
+            c.execute("insert into dirs values ( ? )", [buffer(dirname)])
     conn.commit()
     conn.close()
 
+def connectToServer(ip, port, ipIsSocket=False):
+    sockettype = socket.AF_INET
+    if ipIsSocket: sockettype = socket.AF_UNIX
+    s = socket.socket(sockettype, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    return s
+
+def senddata(s, db):
+    dbdata = open(db,"r")
+    s.sendall(dbdata.read())
+    dbdata.close()
+    while True:
+        filename = ""
+        while True:
+            tmp = s.recv(1024)
+            if not data: break
+            filename += tmp
+        if filename == "EOF": break
+        pointer = open(filename, "r")
+        s.sendall(pointer.read())
+        pointer.close()
+
+def connectioncleanup(s):
+    s.close()
+
 if __name__ == "__main__":
-    index(sys.argv[1])
+    tmpdb = "tmp.db"
+    index(sys.argv[1], tmpdb)
+    s = connectToServer()
+    senddata(s, tmpdb)
+    connectioncleanup(s)
+
+
+    if fail:
+        sys.exit(1)
+    sys.exit(0)
 
